e84397e258fa6686d9d4cced5abd87381c22d572
alefdav <alefdav@gmail.com>
2024-02-27 16:17:14 -0300

    adjusts and build prod

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <nav className="fixed w-screen shadow-xl bg-indigo-600  py-4 px-4">
       <div className="md:container mx-auto flex justify-between">
-        <img src="" alt="logo" />
-        <ul className="flex justify-between w-3/12">
+        <img src="" alt="logo" className="w-2/12" />
+        <ul className="flex justify-between gap-x-6">
           <li>
             <Link href="/">Home</Link>
           </li>
--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,17 @@ export default function Home() {
       <main className="">
         <div className="banner min-h-screen">
           <div className="md:container mx-auto flex justify-center h-screen gap-x-16">
-            <div className="flex justify-center flex-col columns-xs w-1/2">
+            <div className="flex justify-center flex-col columns-xs w-3/4">
               <h1 className="pb-4 text-5xl">Lorem Ipsum</h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Dolores ea dignissimos quaerat ratione possimus veritatis,
-                debitis magnam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quidem, quod deleniti nulla atque molestias omnis officiis porro repellendus dolore harum quis, sapiente voluptatem soluta ea laboriosam earum dolores aspernatur.  
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis suscipit excepturi non natus quo obcaecati! Ex autem saepe in quisquam illum labore vel deleniti iure, perferendis modi dolore odio obcaecati.
               </p>
             </div>
 
             <div className="flex justify-center flex-col columns-xs w-1/2 h-screen">
-              <div className="container mx-auto">
+              <div className="container mx-auto flex flex-col align-middle">
                 <h1 className="text-2xl font-bold mb-4">Lista Aleatória</h1>
                 <ul className="list-disc pl-4">
                   {randomItems.map((item, index) => (
